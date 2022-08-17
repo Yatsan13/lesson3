bd5ebba127e5fec819d7bbbb919608bb006224e1
Risha <browny1320@mail.com>
2022-08-17 14:16:57 +0300

    added first code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+if (length == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+Console.WriteLine($"{number} является палидромом");
+    }
+    else {
+        Console.WriteLine($"{number} не является палидромом");
+    }
+}
+        else
+    {
+        Console.WriteLine($"{number} не является пятизначным числом");
+    }
